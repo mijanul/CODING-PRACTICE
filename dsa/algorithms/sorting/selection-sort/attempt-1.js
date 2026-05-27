89f63d50b1c4f9d620aade16a542aaa2480db091
MijanuL <52299208+mijanul@users.noreply.github.com>
2026-05-27 22:19:41 +0530

    Add Selection Sort implementation with example usage

---
+++ dsa/algorithms/sorting/selection-sort/attempt-1.js
@@ -0,0 +1,21 @@
+const unsortedArr = [1, 8, 2, 4, 5];
+
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+
+    if (minIndex !== i) {
+      let temp = arr[i];
+      arr[i] = arr[minIndex];
+      arr[minIndex] = temp;
+    }
+  }
+  return arr;
+}
+
+console.log(selectionSort(unsortedArr));
