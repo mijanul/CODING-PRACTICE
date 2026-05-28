c8b4545aeb7ab99bf99ca7e6ba405944814d9258
MijanuL <52299208+mijanul@users.noreply.github.com>
2026-05-28 23:28:07 +0530

    implementation of Valid Palindrome attempt 2

---
+++ dsa/strings/valid-palindrome/attempt-2.js
@@ -0,0 +1,19 @@
+function isValidPalindrome(str) {
+  const cleaned = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+
+  let left = 0;
+  let right = cleaned.length - 1;
+
+  while (left < right) {
+    if (cleaned[left] !== cleaned[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+  return true;
+}
+
+console.log(isValidPalindrome("madam")); // true
+console.log(isValidPalindrome("tab a bat")); // true
+console.log(isValidPalindrome("Hello JS"));
