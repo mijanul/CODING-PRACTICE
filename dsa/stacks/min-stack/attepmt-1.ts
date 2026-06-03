eb640bb0d02ba5349d37a01fcba601540b1c7e0f
MijanuL <52299208+mijanul@users.noreply.github.com>
2026-06-03 15:00:21 +0530

    Implement MinStack class with push, pop, top, and getMin methods

---
+++ dsa/stacks/min-stack/attepmt-1.ts
@@ -0,0 +1,47 @@
+class MinStack {
+  private minStack: number[];
+  private stack: number[];
+  constructor() {
+    this.minStack = [];
+    this.stack = [];
+  }
+
+  /**
+   * @param {number} val
+   * @return {void}
+   */
+  push(val: number): void {
+    this.stack.push(val);
+    if (!this.minStack.length) {
+      this.minStack.push(val);
+    } else {
+      if (this.minStack[this.minStack.length - 1] >= val) {
+        this.minStack.push(val);
+      }
+    }
+  }
+
+  /**
+   * @return {void}
+   */
+  pop(): void {
+    let popped = this.stack.pop();
+    if (popped === this.minStack[this.minStack.length - 1]) {
+      this.minStack.pop();
+    }
+  }
+
+  /**
+   * @return {number}
+   */
+  top(): number {
+    return this.stack[this.stack.length - 1];
+  }
+
+  /**
+   * @return {number}
+   */
+  getMin(): number {
+    return this.minStack[this.minStack.length - 1];
+  }
+}
