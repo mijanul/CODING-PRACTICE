9c3772ba61872dbb1c6be31fdbc9f05dac6bb602
MijanuL <52299208+mijanul@users.noreply.github.com>
2026-06-05 18:34:28 +0530

    feat: implement rotate array function with in-place modification

---
+++ dsa/arrays/rotate-array/attempt-1-s1.ts
@@ -0,0 +1,21 @@
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+function rotate(nums: number[], k: number): void {
+  let n = nums.length;
+  k %= n;
+
+  const arr: number[] = new Array(n);
+
+  for (let i = 0; i < n; i++) {
+    arr[(i + k) % n] = nums[i];
+  }
+
+  for (let i = 0; i < n; i++) {
+    nums[i] = arr[i];
+  }
+
+  console.log(arr);
+}
+
+rotate([1, 2, 3, 4, 5, 6, 7], 3);
