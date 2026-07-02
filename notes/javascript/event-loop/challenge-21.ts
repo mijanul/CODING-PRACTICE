0760d7a9afe53f34cbd9eb4f8b04a2d52c2abe10
MijanuL <52299208+mijanul@users.noreply.github.com>
2026-07-02 17:56:38 +0530

    feat: add challenge 21 example demonstrating async/await and event loop behavior in JavaScript

---
+++ notes/javascript/event-loop/challenge-21.ts
@@ -0,0 +1,29 @@
+console.log("A");
+
+async function foo() {
+  console.log("B");
+
+  await Promise.resolve().then(() => {
+    console.log("C");
+
+    return Promise.resolve().then(() => {
+      console.log("D");
+    });
+  });
+
+  console.log("E");
+}
+
+foo();
+
+Promise.resolve().then(() => {
+  console.log("F");
+});
+
+queueMicrotask(() => {
+  console.log("G");
+});
+
+console.log("H");
+
+export {};
