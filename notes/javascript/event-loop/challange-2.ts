a5788b5ee3e8a4576f40bb1fa7f164904eca4034
MijanuL <52299208+mijanul@users.noreply.github.com>
2026-07-02 17:36:29 +0530

    feat: add initial implementation of event loop examples in JavaScript

---
+++ notes/javascript/event-loop/challange-2.ts
@@ -0,0 +1,33 @@
+console.log("A");
+
+async function foo() {
+  console.log("B");
+
+  await Promise.resolve().then(() => {
+    console.log("C");
+
+    return Promise.resolve().then(() => {
+      console.log("D");
+
+      return "DONE";
+    });
+  });
+
+  console.log("E");
+}
+
+foo();
+
+Promise.resolve().then(() => {
+  console.log("F");
+
+  return Promise.resolve().then(() => {
+    console.log("G");
+  });
+});
+
+queueMicrotask(() => {
+  console.log("H");
+});
+
+console.log("I");
