8ae342dad6a7bddfebe1b46394cac4288a1082c0
MijanuL <52299208+mijanul@users.noreply.github.com>
2026-07-02 17:39:53 +0530

    feat: add challenge 2 and challenge 4 examples for event loop in JavaScript

---
+++ notes/javascript/event-loop/challenge-4.ts
@@ -0,0 +1,21 @@
+console.log(1);
+
+setTimeout(() => {
+  console.log(2);
+
+  Promise.resolve().then(() => {
+    console.log(3);
+  });
+
+  console.log(4);
+}, 0);
+
+Promise.resolve().then(() => {
+  console.log(5);
+
+  setTimeout(() => {
+    console.log(6);
+  }, 0);
+});
+
+console.log(7);
