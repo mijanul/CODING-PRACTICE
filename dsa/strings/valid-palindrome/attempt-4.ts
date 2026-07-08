e089b3e27ba8a628dab989c9b8ad3b8261f4dcdf
MijanuL <52299208+mijanul@users.noreply.github.com>
2026-07-08 22:39:31 +0530

    feat: implement isPalindrome function to check if a string is a palindrome

---
+++ dsa/strings/valid-palindrome/attempt-4.ts
@@ -0,0 +1,20 @@
+function isPalindrome(s: string): boolean {
+  s = s.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left < right) {
+    if (s[left] !== s[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+
+  return true;
+}
+
+export {};
+
+console.log(isPalindrome("Was it a car or a cat I saw?"));
