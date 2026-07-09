74884bfef808362a5d2b90cbbc15ff99eeb09b74
MijanuL <52299208+mijanul@users.noreply.github.com>
2026-07-09 14:28:43 +0530

    feat: implement lengthOfLongestSubstring function to find the length of the longest substring without repeating characters

---
+++ dsa/strings/longest-substring-without-repeating-characters/practice-2.ts
@@ -0,0 +1,22 @@
+function lengthOfLongestSubstring(s: string): number {
+  const set: Set<string> = new Set();
+  let longestLen = 0;
+  let left = 0;
+
+  for (let right = 0; right < s.length; right++) {
+    while (set.has(s[right])) {
+      set.delete(s[left]);
+      left++;
+    }
+
+    set.add(s[right]);
+
+    longestLen = Math.max(longestLen, right - left + 1);
+  }
+
+  return longestLen;
+}
+
+export {};
+
+console.log(lengthOfLongestSubstring("abcabcbb")); // 3
