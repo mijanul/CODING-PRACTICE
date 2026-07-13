d083a4236074aef4b5f9897f7c9f209acfbb8272
MijanuL <52299208+mijanul@users.noreply.github.com>
2026-07-13 22:29:49 +0530

    feat: implement isPalindrome function to check if a number is a palindrome

---
+++ dsa/math/palindrome-number/solution-1.ts
@@ -0,0 +1,18 @@
+function isPalindrome(x: number): boolean {
+  if (x < 0 || (x % 10 === 0 && x !== 0)) return false;
+  let num = x;
+  let reverse = 0;
+
+  while (num > 0) {
+    let lastDigit = num % 10;
+    reverse = reverse * 10 + lastDigit;
+    num = Math.floor(num / 10);
+  }
+
+  return reverse === x;
+}
+
+export {};
+
+console.log(isPalindrome(121)); // true
+console.log(isPalindrome(-121)); // false
